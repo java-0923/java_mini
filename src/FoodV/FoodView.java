c55a6dbb1409e10abd23c69440ff54a68d879780
20230720 <ghkdwo34@naver.com>
2023-10-05 16:03:31 +0900

    FoodView 기초(오타수정)

--- src/FoodV/FoodView.java
+++ src/FoodV/FoodView.java
@@ -13,10 +13,10 @@ public class FoodView {
     public static void start() {
         while (true) {
             System.out.println("\n\n****** 음식 주문 프로그램 ******");
-            System.out.printf("# 현재 등록된 주문: %d명\n", ar.count());
+            System.out.printf("$ 현재 등록된 주문 $ : %d명\n", ar.count());
             System.out.println("* 1. 팝콘 주문 하기");
             System.out.println("# 2. 음료수 주문 하기");
-            System.out.println("# 3. 사이드 메뉴 주문 하기");
+            System.out.println("% 3. 사이드 메뉴 주문 하기");
 
             makeLine();
 
@@ -41,9 +41,9 @@ public class FoodView {
     private static void insertProcess() {
 
         System.out.println("\n# 메뉴 등록을 시작합니다.");
-        String popconName = input("- 팝콘: ");
-        String drinkName = input("- 음료수: ");
-        String sideName = input("- 음료수: ");
+        String popconName = input("* 팝콘 *: ");
+        String drinkName = input("* 음료수 *: ");
+        String sideName = input("* 사이드 메뉴 * : ");
 
         // 기존에 등록된 주문자 인가?
         if (!ar.isRegistered(popconName)) { // 등록되지 않은 신규 주문자라면
