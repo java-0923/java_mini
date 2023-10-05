86dd82840ce679ef816376b15cce45cf67a94b21
20230720 <ghkdwo34@naver.com>
2023-10-05 14:57:11 +0900

    FoodView 기초

---
+++ src/FoodV/FoodRepository.java
@@ -0,0 +1,21 @@
+package FoodV;
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class FoodRepository {
+
+
+    public boolean isRegistered(String popconName) {
+    }
+
+    public void autoSave() {
+    }
+
+    public boolean addNewMenu(String popconName, String drinkName) {
+    }
+
+    public Object count() {
+    }
+}
